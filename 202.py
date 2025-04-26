a6dd66c0b69cca481fda6cd189f29d10bfdc6342
swsxm <129607562+swsxm@users.noreply.github.com>
2025-04-26 14:15:39 +0200

    comment

--- 202.py
+++ 202.py
@@ -3,6 +3,8 @@ class Solution:
         lookup = set()
         while n not in lookup:
             lookup.add(n)
+            # if no cast to string allowed
+            # use divmod...
             n = sum(int(num) ** 2 for num in str(n))
             if n == 1:
                 return True
