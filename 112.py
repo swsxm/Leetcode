870fdf325b40de0ab29242da3e421a21be36d2db
swsxm <129607562+swsxm@users.noreply.github.com>
2025-05-05 16:32:42 +0200

    new

---
+++ 112.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        if not root:
+            return False
+
+        def dfs(root, targetSum):
+            if not root:
+                return False
+            targetSum -= root.val
+            if not root.left and not root.right:
+                return targetSum == 0
+            return dfs(root.left, targetSum) or dfs(root.right, targetSum)
+
+        return dfs(root, targetSum)
